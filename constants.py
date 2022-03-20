103fbd49ec1f89ac6582b31e16d987791cd75e65
Antonino Vespoli <antoninovespoli@eaton.com>
2022-03-20 17:19:23 +0000

    Added file that includes all project constants

---
+++ constants.py
@@ -0,0 +1,18 @@
+'''
+This module includes all the constants of the project.
+
+author: Antonino Vespoli
+date: March 20, 2022
+'''
+
+CHURN_DISTRIBUTION_IMG_PATH = 'images/eda/churn_distribution.png'
+CUSTOMER_AGE_DISTRIBUTION_IMG_PATH = 'images/eda/customer_age_distribution.png'
+MARITAL_DISTRIBUTION_IMG_PATH = 'images/eda/marital_status_distribution.png'
+TOTAL_TRANSACTION_IMG_PATH = 'images/eda/total_transaction_distribution.png'
+HEATMAP_IMG_PATH = 'images/eda/heatmap.png'
+RFC_MODEL_PATH = './models/rfc_model.pkl'
+LOGISTIC_MODEL_PATH = './models/logistic_model.pkl'
+ROC_CURVE_RES_IMG_PATH = 'images/results/roc_curve_results.png'
+FEATURE_IMPORT_IMG_PATH = './images/results/feature_importances.png'
+RF_RES_IMG_PATH = 'images/results/rf_results.png'
+LOGISTIC_RES_IMG_PATH = 'images/results/logistic_results.png'
